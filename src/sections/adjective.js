4fa9151cd55c4bfc47670e3181a4694b7260a137
Asier Aduriz <aaduriz@vistaprint.com>
2020-10-18 21:05:58 +0200

    Add state to adjective section

--- src/sections/adjective.js
+++ src/sections/adjective.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Text, TextInput, View } from 'react-native';
 import { useSettingsContext } from '../contexts/SettingsContext';
+
+const adjectiveReducer = (state, { type, value }) => {
+  state[type] = value;
+  return state;
+}
 
 export const AdjectiveSection = () => {
   const { settingsState } = useSettingsContext();
   const { genders } = settingsState;
 
+  const [state, dispatch] = useReducer(adjectiveReducer, {});
+
   return (
     <View key='adjective-view' style={{ marginTop: 10, marginBottom: 10 }}>
       <View key='genders'>
-        <Text>Add genders here {genders}</Text>
+        {
+          genders.map((gender) => {
+            return (
+              <View key={`gender-${gender}`}>
+                <Text>{gender}</Text>
+                <TextInput
+                  value={state[gender]}
+                  onChangeText={(text) => dispatch({ type: gender, value: text })}
+                />
+              </View>
+            )
+          })
+        }
       </View>
     </View>
   )
