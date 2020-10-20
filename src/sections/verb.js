18278aeffa9902efc4f6af04910ea6aab4feead9
Asier Aduriz <aaduriz@vistaprint.com>
2020-10-20 23:23:22 +0200

    Silly collapse verb

--- src/sections/verb.js
+++ src/sections/verb.js
@@ -1,7 +1,7 @@
 import { Picker } from '@react-native-community/picker';
-
+import { AntDesign } from '@expo/vector-icons';
 import React, { useReducer, useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { useSettingsContext } from '../contexts/SettingsContext';
 
 const verbReducer = (state, { tense, subject, value }) => {
@@ -10,6 +10,10 @@ const verbReducer = (state, { tense, subject, value }) => {
 }
 
 export const VerbSection = () => {
+  const [expanded, setExpanded] = useState(true);
+  const caretDown = <AntDesign onPress={() => setExpanded(!expanded)} name="caretdown" size={24} color="black" />
+  const caretRight = <AntDesign onPress={() => setExpanded(!expanded)} name="caretright" size={24} color="black" />
+
   const { settingsState } = useSettingsContext();
   const { tenses, subjects } = settingsState;
   const firstTense = tenses[0];
@@ -19,24 +23,53 @@ export const VerbSection = () => {
   tenses.forEach(tense => initialState[tense] = {});
   const [state, dispatch] = useReducer(verbReducer, initialState);
 
-  return (
-    <View key='verb-view' style={{ marginTop: 10, marginBottom: 10 }}>
-      <Picker
-        selectedValue={tense}
-        onValueChange={setTense}
-      >
-        {tenses.map((tense, index) => <Picker.Item key={`${tense}-${index}`} label={tense} value={tense} />)}
-      </Picker>
+  return expanded ?
 
-      {subjects.map((subject) =>
-        <View key={`${tense}-${subject}`}>
-          <Text>{subject}</Text>
-          <TextInput
-            value={state[tense][subject]}
-            onChangeText={(text) => dispatch({ tense, subject, value: text })}
-          />
-        </View>
-      )}
+    <View key='verb-view' style={{ marginTop: 10, marginBottom: 10 }}>
+      <View style={{ flexDirection: "row-reverse" }}>
+        {caretDown}
+      </View>
+      <View key='verb-picker' style={styles.bottomLine}>
+        <Picker
+          selectedValue={tense}
+          onValueChange={setTense}
+        >
+          {tenses.map((tense, index) => <Picker.Item key={`${tense}-${index}`} label={tense} value={tense} />)}
+        </Picker>
+      </View>
+      {
+        subjects.map((subject) =>
+          <View key={`${tense}-${subject}`} style={styles.inputContainer}>
+            <Text style={styles.inputContainerLabel}>{subject}</Text>
+            <TextInput
+              style={styles.inputContainerTextInput}
+              value={state[tense][subject]}
+              onChangeText={(text) => dispatch({ tense, subject, value: text })}
+            />
+          </View>
+        )
+      }
     </View>
-  )
+    :
+    caretRight
+
 }
+
+const styles = StyleSheet.create({
+  bottomLine: {
+    borderBottomWidth: 1
+  },
+  inputContainer: {
+    flexDirection: "row",
+    paddingLeft: 15,
+  },
+  inputContainerLabel: {
+    marginTop: 8,
+    width: '25%'
+  },
+  inputContainerTextInput: {
+    height: 40,
+    width: '70%',
+    borderBottomWidth: 1
+  }
+})
