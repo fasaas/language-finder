881b5887686c2d97d70de09049c6ae2dcc387cb1
Asier Aduriz <aaduriz@vistaprint.com>
2020-10-20 23:48:34 +0200

    Silly way of removing phrases

--- src/screens/NewNoteScreen.js
+++ src/screens/NewNoteScreen.js
@@ -8,12 +8,26 @@ import { VerbSection } from '../sections/verb';
 
 const availableSections = ['Sentence', 'Adjective', 'Verb'];
 
+let componentsCount = 0;
 export const NewNoteScreen = () => {
   const { settingsState } = useSettingsContext();
 
   const [title, setTitle] = useState();
   const [selectedSection, setSelectedSection] = useState(availableSections[0]);
-  const [sections, setSections] = useState([<PhraseSection />]);
+
+  const remove = (id) => {
+    const filteredSections = sections.filter((section) => section.id !== id);
+    setSections(filteredSections);
+  }
+
+  const [sections, setSections] = useState([{ id: 0, component: <PhraseSection id={componentsCount} deleteMe={remove} /> }]);
+
+  const push = (Component) => {
+    componentsCount++;
+    setSections(sections.concat({ id: componentsCount, component: <Component id={componentsCount} deleteMe={remove} /> }))
+  };
+
+
 
   return (
     <ScrollView>
@@ -27,7 +41,7 @@ export const NewNoteScreen = () => {
       </View>
       {
         sections.length > 0
-          ? sections.map((Component, index) => <View key={`${title}-sections-${index}`}>{Component}</View>)
+          ? sections.map((element, index) => <View key={`${title}-sections-${index}`}>{element.component}</View>)
           : null
       }
       <View key='add-section'>
@@ -43,9 +57,9 @@ export const NewNoteScreen = () => {
         <Button
           title='Add note'
           onPress={() => {
-            if (selectedSection === 'Sentence') setSections(sections.concat(<PhraseSection />))
-            if (selectedSection === 'Adjective') setSections(sections.concat(<AdjectiveSection />))
-            if (selectedSection === 'Verb') setSections(sections.concat(<VerbSection />))
+            if (selectedSection === 'Sentence') push(PhraseSection)
+            if (selectedSection === 'Adjective') push(AdjectiveSection)
+            if (selectedSection === 'Verb') push(VerbSection)
           }}
         />
       </View>
--- src/sections/phrase.js
+++ src/sections/phrase.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import { TextInput, View } from 'react-native';
 import { Collapsible } from '../components/collapsible';
+import { AntDesign } from '@expo/vector-icons';
 
-export const PhraseSection = () => {
+
+export const PhraseSection = ({ id, deleteMe }) => {
   const [from, setFrom] = useState();
   const [to, setTo] = useState();
 
   return (
-    <Collapsible style={{ marginTop: 10, marginBottom: 10 }}>
+    <Collapsible style={{ marginTop: 10, marginBottom: 10 }}
+    >
+      <View style={{ flexDirection: "row-reverse" }}>
+        <AntDesign style={{ width: '30%' }} name="delete" size={24} color="black" onPress={() => deleteMe(id)} />
+      </View>
       <View key='from-sentence'>
         <TextInput
           style={{ borderColor: 'gray', borderWidth: 1 }}
