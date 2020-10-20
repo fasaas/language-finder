35e39f6ac5ffbea1abd7e5a9438c8f513052c95a
Asier Aduriz <aaduriz@vistaprint.com>
2020-10-20 23:26:26 +0200

    Extract collapsible to a separate component

---
+++ src/components/collapsible.js
@@ -0,0 +1,22 @@
+import { AntDesign } from '@expo/vector-icons';
+import React, { useReducer } from 'react';
+import { View } from 'react-native';
+
+const reduce = (state) => !state
+const initializeExpanded = true;
+
+export const Collapsible = ({ children }) => {
+  const [expanded, toggle] = useReducer(reduce, initializeExpanded);
+
+  return expanded ?
+    <View>
+      <View style={{ flexDirection: "row-reverse" }}>
+        <AntDesign onPress={toggle} name="caretdown" size={24} color="black" />
+      </View>
+      {children}
+    </View>
+    :
+    <View style={{ flexDirection: "row-reverse" }}>
+      <AntDesign onPress={toggle} name="caretright" size={24} color="black" />
+    </View>
+}
--- src/sections/verb.js
+++ src/sections/verb.js
@@ -1,8 +1,8 @@
 import { Picker } from '@react-native-community/picker';
-import { AntDesign } from '@expo/vector-icons';
 import React, { useReducer, useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { useSettingsContext } from '../contexts/SettingsContext';
+import { Collapsible } from '../components/collapsible';
 
 const verbReducer = (state, { tense, subject, value }) => {
   state[tense][subject] = value;
@@ -10,10 +10,6 @@ const verbReducer = (state, { tense, subject, value }) => {
 }
 
 export const VerbSection = () => {
-  const [expanded, setExpanded] = useState(true);
-  const caretDown = <AntDesign onPress={() => setExpanded(!expanded)} name="caretdown" size={24} color="black" />
-  const caretRight = <AntDesign onPress={() => setExpanded(!expanded)} name="caretright" size={24} color="black" />
-
   const { settingsState } = useSettingsContext();
   const { tenses, subjects } = settingsState;
   const firstTense = tenses[0];
@@ -23,36 +19,32 @@ export const VerbSection = () => {
   tenses.forEach(tense => initialState[tense] = {});
   const [state, dispatch] = useReducer(verbReducer, initialState);
 
-  return expanded ?
-
-    <View key='verb-view' style={{ marginTop: 10, marginBottom: 10 }}>
-      <View style={{ flexDirection: "row-reverse" }}>
-        {caretDown}
+  return (
+    <Collapsible>
+      <View key='verb-view' style={{ marginTop: 10, marginBottom: 10 }}>
+        <View key='verb-picker' style={styles.bottomLine}>
+          <Picker
+            selectedValue={tense}
+            onValueChange={setTense}
+          >
+            {tenses.map((tense, index) => <Picker.Item key={`${tense}-${index}`} label={tense} value={tense} />)}
+          </Picker>
+        </View>
+        {
+          subjects.map((subject) =>
+            <View key={`${tense}-${subject}`} style={styles.inputContainer}>
+              <Text style={styles.inputContainerLabel}>{subject}</Text>
+              <TextInput
+                style={styles.inputContainerTextInput}
+                value={state[tense][subject]}
+                onChangeText={(text) => dispatch({ tense, subject, value: text })}
+              />
+            </View>
+          )
+        }
       </View>
-      <View key='verb-picker' style={styles.bottomLine}>
-        <Picker
-          selectedValue={tense}
-          onValueChange={setTense}
-        >
-          {tenses.map((tense, index) => <Picker.Item key={`${tense}-${index}`} label={tense} value={tense} />)}
-        </Picker>
-      </View>
-      {
-        subjects.map((subject) =>
-          <View key={`${tense}-${subject}`} style={styles.inputContainer}>
-            <Text style={styles.inputContainerLabel}>{subject}</Text>
-            <TextInput
-              style={styles.inputContainerTextInput}
-              value={state[tense][subject]}
-              onChangeText={(text) => dispatch({ tense, subject, value: text })}
-            />
-          </View>
-        )
-      }
-    </View>
-    :
-    caretRight
-
+    </Collapsible>
+  )
 }
 
 const styles = StyleSheet.create({
